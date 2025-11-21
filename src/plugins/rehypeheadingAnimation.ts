8f84fefea49f95ffd3df197b8d341f00de5e1574
Benjamin Caradeuc <bencrdc@gmail.com>
2025-11-21 17:34:43 +0100

    updated astro & sass + astro check

--- src/plugins/rehypeheadingAnimation.ts
+++ src/plugins/rehypeheadingAnimation.ts
@@ -1,17 +1,18 @@
 import { visit } from 'unist-util-visit';
+import type { Root, Element } from 'hast';
 
 export default function rehypeHeadingAnimation() {
-  return (tree) => {
-    visit(tree, 'element', (node) => {
-      if (['h1', 'h2', 'h3', 'h4'].includes(node.tagName)) {
-        node.properties = node.properties || {};
-        node.properties['scroll-animation'] = 'slide-inline-start';
-      }
+    return (tree: Root) => {
+        visit(tree, 'element', (node: Element) => {
+            if (['h1', 'h2', 'h3', 'h4'].includes(node.tagName)) {
+                node.properties = node.properties || {};
+                node.properties['scroll-animation'] = 'slide-inline-start';
+            }
 
-      if (['img', 'iframe', 'pre', 'blockquote'].includes(node.tagName)) {
-        node.properties = node.properties || {};
-        node.properties['scroll-animation'] = 'scale';
-      }
-    });
-  };
+            if (['img', 'iframe', 'pre', 'blockquote'].includes(node.tagName)) {
+                node.properties = node.properties || {};
+                node.properties['scroll-animation'] = 'scale';
+            }
+        });
+    };
 }
